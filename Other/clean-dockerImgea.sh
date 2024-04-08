1c59a14496d83a3ba930aca1ddb644975d0fb5f1
bai784724280 <58358565+bai784724280@users.noreply.github.com>
2024-04-08 08:57:57 +0800

    升级 mybatis依赖 版本

--- Other/clean-dockerImgea.sh
+++ Other/clean-dockerImgea.sh
@@ -4,17 +4,17 @@
 IMAGE_NAME="tinkerplay"
 IMAGE_TAG="0.0.1-SNAPSHOT"
 
-# 检查镜像是否存在
-if docker image inspect "${IMAGE_NAME}:${IMAGE_TAG}" &> /dev/null; then
-    echo "Image exists. Cleaning up..."
-
     # 查找并停止相关容器
-    CONTAINERS=$(docker ps -q --filter "ancestor=${IMAGE_NAME}:${IMAGE_TAG}")
+    CONTAINERS=$(docker ps -aq --filter "ancestor=${IMAGE_NAME}:${IMAGE_TAG}")
     if [ -n "$CONTAINERS" ]; then
         docker stop $CONTAINERS
         docker rm $CONTAINERS
         echo "Stopped and removed containers."
     fi
+
+# 检查镜像是否存在
+if docker image inspect "${IMAGE_NAME}:${IMAGE_TAG}" &> /dev/null; then
+    echo "Image exists. Cleaning up..."
 
     # 删除镜像
     docker rmi "${IMAGE_NAME}:${IMAGE_TAG}"
