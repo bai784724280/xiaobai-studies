a96aa40bee3159a66e2445527d1b29df5acfb377
yuxinbai <784724280@qq.com>
2024-07-10 21:16:27 +0800

    研究一点是一点

---
+++ src/main/java/com/aiyuns/tinkerplay/Other/Demo/ExceptionTest.java
@@ -0,0 +1,25 @@
+package com.aiyuns.tinkerplay.Other.Demo;
+
+public class ExceptionTest {
+
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        try {
+            System.out.println(count++ + " :try块异常语句执行之前");
+            int i = 10 / 0;
+            System.out.println(count++ + " :try块异常语句执行之后");
+        } catch (Exception e) {
+            System.out.println(count++ + " :catch块执行");
+        } finally {
+            System.out.println(count++ + " :finally块执行");
+        }
+        throwstest();
+    }
+
+    public static void throwstest() throws Exception {
+        System.out.println(count++ + " :throws异常语句执行之前");
+        int i = 1/0;
+        System.out.println(count++ + " :throws异常语句执行之后");
+    }
+}
