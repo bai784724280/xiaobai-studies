9955a4015892fce254338c686cb9d643c6575d24
yuxinbai <784724280@qq.com>
2024-07-11 19:52:00 +0800

    模拟转账业务

--- src/main/java/com/aiyuns/tinkerplay/Controller/Service/ServiceImpl/TransferServiceImpl.java
+++ src/main/java/com/aiyuns/tinkerplay/Controller/Service/ServiceImpl/TransferServiceImpl.java
@@ -3,6 +3,7 @@ package com.aiyuns.tinkerplay.Controller.Service.ServiceImpl;
 import com.aiyuns.tinkerplay.Controller.Elasticsearch.Repository.AccountRepository;
 import com.aiyuns.tinkerplay.Controller.Service.TransferService;
 import com.aiyuns.tinkerplay.Entity.Account;
+import com.baomidou.dynamic.datasource.annotation.DS;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class TransferServiceImpl implements TransferService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @DS("slave_1")
     @Transactional
     public void transfer(String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
         Account fromAccount = accountRepository.findByAccountNumber(fromAccountNumber)
