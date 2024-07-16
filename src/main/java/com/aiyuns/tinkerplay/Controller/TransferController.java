b823e716ab3139a587ca7e80101782d82b051a7f
yuxinbai <784724280@qq.com>
2024-07-16 19:40:41 +0800

    修改项目目录结构

--- src/main/java/com/aiyuns/tinkerplay/Controller/TransferController.java
+++ src/main/java/com/aiyuns/tinkerplay/Controller/TransferController.java
@@ -1,7 +1,9 @@
 package com.aiyuns.tinkerplay.Controller;
 
 import com.aiyuns.tinkerplay.Controller.Service.TransferService;
+import com.aiyuns.tinkerplay.CustomAnnotations.WebLog;
 import com.aiyuns.tinkerplay.Entity.TransferRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ public class TransferController {
     private TransferService transferService;
 
     @PostMapping
+    @Operation(summary = "模拟转账")
+    @WebLog(description = "模拟转账")
     public ResponseEntity<String> transfer(@RequestBody TransferRequest transferRequest) {
         transferService.transfer(transferRequest.getFromAccountNumber(),
                 transferRequest.getToAccountNumber(),
