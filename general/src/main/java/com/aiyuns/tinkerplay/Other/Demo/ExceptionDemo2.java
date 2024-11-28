5ac5d806633544f0a3687a44aca8182bcd0b0571
yuxinbai <784724280@qq.com>
2024-11-28 14:43:20 +0800

    mybatis-spring 3.0.4

--- general/src/main/java/com/aiyuns/tinkerplay/Other/Demo/ExceptionDemo2.java
+++ general/src/main/java/com/aiyuns/tinkerplay/Other/Demo/ExceptionDemo2.java
@@ -3,19 +3,21 @@ package com.aiyuns.tinkerplay.Other.Demo;
 public class ExceptionDemo2 {
 
     public static void main(String[] args) throws Exception{
-        String value = "";
+        String value = "1";
         try {
             throw new Exception("Primary Exception");
         } catch (Exception e) {
             try {
                 //int i = 1/0;
-                value = "xxx";
+                value = "2";
             } catch (Exception ex) {
                 System.out.println("抛出内部异常");
                 throw ex;
             }
             System.out.println("抛出外部异常");
             throw e;
+        } finally {
+            System.out.println(value);
         }
     }
 }
